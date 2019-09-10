6e61eaa8978d5dab1f9a55a0b9919fb710a8261a
Tobias Guggenmos <tguggenm@redhat.com>
2019-09-10 20:10:26 +0200

    Add a simple LSP client
    Will launch a (not yet existing) server and consult it for all .promql
    files.
    Fixes #4.

--- src/extension.ts
+++ src/extension.ts
@@ -2,22 +2,62 @@
 // FIXME: restrict import to what is actually needed
 import * as vscode from 'vscode';
 import * as lspclient from 'vscode-languageclient';
+import * as path from 'path';
+import { Server } from 'http';
 
 let client: lspclient.LanguageClient;
 
 
+// tslint:disable-next-line: typedef
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Congratulations, your extension "vscode-prometheus" is now active!');
+	console.log('Your extension "vscode-prometheus" is now active!');
 
 	// The command has been defined in the package.json file
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.usePromQLLangServer', () => {
-		vscode.window.showInformationMessage('Not implemented yet :(');
-	});
+	let disposable: lspclient.Disposable =
+		vscode.commands.registerCommand('extension.usePromQLLangServer', () => {
+			vscode.window.showInformationMessage('Not implemented yet :(');
+		});
 
 	context.subscriptions.push(disposable);
+
+
+	let serverExec: lspclient.Executable = {
+		command: context.asAbsolutePath(path.join('path', 'to', 'langserver')),
+		args: []
+	};
+	// Only used when extension is launched in debug mode
+
+	let serverExecDebug: lspclient.Executable = {
+		command: context.asAbsolutePath(path.join('path', 'to', 'langserver')),
+		args: ['--verbose']
+	};
+
+	let serverOptions: lspclient.ServerOptions = {
+		run: serverExec,
+		debug: serverExecDebug
+	};
+
+	let clientOptions: lspclient.LanguageClientOptions = {
+		documentSelector: [{ scheme: 'file', language: 'promql' }],
+		synchronize: {
+			// This can be used as a config file later 
+			fileEvents: vscode.workspace.createFileSystemWatcher('**/.promql-lsp.json')
+		}
+	};
+
+	client = new lspclient.LanguageClient(
+    'promql-lsp-client',
+    'PromQL Language Server',
+    serverOptions,
+    clientOptions
+  );
+
+  client.start();
+
 }
 
 
-export function deactivate() {}
+// tslint:disable-next-line: typedef
+export function deactivate() { }
