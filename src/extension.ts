6ffbc978ef39368f129150f797d6eec2f32f9cc3
Tobias Guggenmos <tguggenm@redhat.com>
2019-09-10 18:22:06 +0200

    Add a dummy filetype to trigger the extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,23 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// FIXME: restrict import to what is actually needed
 import * as vscode from 'vscode';
+import * as lspclient from 'vscode-languageclient';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let client: lspclient.LanguageClient;
+
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-prometheus" is now active!');
 
 	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.usePromQLLangServer', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Not implemented yet :(');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
+
 export function deactivate() {}
